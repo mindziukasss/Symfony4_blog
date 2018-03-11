9ec2f429d20042d7f1abfdfa468f667502b03ba1
mindziukasss <mindziukasss@gmail.com>
2018-03-11 14:13:42 +0200

    Added crate post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,23 +2,73 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * Class PostController
  * @package App\Controller
- * @Route("/post")
+ * @Route("/post", name="post")
  */
 
 
 class PostController extends Controller
 {
     /**
-     * @Route("/", name="post")
+     * @Route("/", name="index")
      */
     public function indexAction()
     {
         return $this->render('post/index.html.twig');
     }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function createAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = new Post();
+
+        /**
+         * @var Form
+         */
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            /**
+             * @var UploadedFile $file
+             */
+
+            $file = $post->getImage();
+
+            $fileName = md5(uniqid()).'.'.$file->guessClientExtension();
+
+            $file->move(
+              $this->getParameter('images_directory'),
+              $fileName
+            );
+
+            $post->setImage($fileName);
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('postindex'));
+        }
+
+        return $this->render('post/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,10 @@ class PostType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('image', FileType::class)
+            ->add(
+                'save',
+                SubmitType::class
+            )
         ;
     }
 
