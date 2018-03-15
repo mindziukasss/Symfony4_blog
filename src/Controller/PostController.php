217b6484ab284d3eeafe35ee475f8f0d436a8624
mindziukasss <mindziukasss@gmail.com>
2018-03-15 16:15:07 +0200

    Added edit post method and change show

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,7 +7,6 @@ use App\Form\PostType;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -16,8 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @package App\Controller
  * @Route("/posts", name="posts")
  */
-
-
 class PostController extends Controller
 {
     /**
@@ -31,7 +28,7 @@ class PostController extends Controller
 
         $posts = $repository->findAll();
 
-        return $this->render('post/index.html.twig',[
+        return $this->render('post/index.html.twig', [
             'posts' => $posts
         ]);
     }
@@ -51,44 +48,86 @@ class PostController extends Controller
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             /**
              * @var UploadedFile $file
              */
             $file = $post->getImage();
-            $fileName = md5(uniqid()).'.'.$file->guessClientExtension();
+            $fileName = md5(uniqid()) . '.' . $file->guessClientExtension();
             $file->move(
-              $this->getParameter('images_directory'),
-              $fileName
+                $this->getParameter('images_directory'),
+                $fileName
             );
             $post->setImage($fileName);
             $em->persist($post);
             $em->flush();
-            return $this->redirect($this->generateUrl('postindex'));
+            return $this->redirect($this->generateUrl('postsindex'));
         }
         return $this->render('post/new.html.twig', [
-            'form' => $form->createView()
+            'form_new_post' => $form->createView()
         ]);
 
     }
 
     /**
      * @Route("/{id}")
-     * @param Post $post
-     * @param SessionInterface $session
+     * @param $id
      * @return \Symfony\Component\HttpFoundation\Response
-     * @internal param SessionInterface $sessio
      */
 
-    public function showAction(Post $post, SessionInterface $session)
+    public function showAction($id)
     {
-
-        $session->set('id', $post->getId());
+        $post = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->find($id);
 
         return $this->render('post/show.html.twig', [
             'post' => $post,
         ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em
+            ->getRepository(Post::class)
+            ->find($id);
+        if (!$post) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var UploadedFile $file
+             */
+            $file = $post->getImage();
+            $fileName = md5(uniqid()) . '.' . $file->guessClientExtension();
+            $file->move(
+                $this->getParameter('images_directory'),
+                $fileName
+            );
+            $post->setImage($fileName);
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute("postsindex");
+        }
+
+        return $this->render("post/edit.html.twig", [
+            'form_edit_post' => $form->createView()
+        ]);
+
+
     }
 
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,6 +6,7 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,20 +16,23 @@ class PostType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description')
-            ->add('image', FileType::class)
+            ->add('description', TextareaType::class)
+            ->add('image', FileType::class, array(
+                'label' => 'Image',
+                'data_class' => null,
+                'required' => false
+            ))
             ->add(
                 'save',
                 SubmitType::class
-            )
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             // uncomment if you want to bind to a class
-            //'data_class' => Post::class,
+            'data_class' => Post::class,
         ]);
     }
 }
